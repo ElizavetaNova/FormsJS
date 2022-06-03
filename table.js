f657e8e24793fac4d18bdc5bd2c058a908855343
Elizaveta <liza_fahrut@mail.ru>
2022-06-03 14:01:49 +1000

    испрвила считывание данных с сервера

--- table.js
+++ table.js
@@ -1,41 +1,18 @@
-import myJSON from '/json/data.json' assert { type: 'json' };
-console.log(myJSON);
+const myJSON = await fetch('/json/data.json');
+const data = await myJSON.json();
 
-Array.from(myJSON.members).forEach(member => {
+Array.from(data.members).forEach(member => {
     const tr = document.createElement('tr');
-    let td1 = document.createElement('td')
-    td1.innerHTML = member.firstNameRider + ' ' + member.lastNameRider
-    tr.appendChild(td1);
-    let td2 = document.createElement('td')
-    td2.innerHTML = member.dateBirthdayRider
-    tr.appendChild(td2);
-    let td3 = document.createElement('td')
-    td3.innerHTML = member.phone
-    tr.appendChild(td3);
-    let td4 = document.createElement('td')
-    td4.innerHTML = member.email
-    tr.appendChild(td4);
-    let td5 = document.createElement('td')
-    td5.innerHTML = member.nameHorse
-    tr.appendChild(td5);
-    let td6 = document.createElement('td')
-    td6.innerHTML = member.ageHorse
-    tr.appendChild(td6);
-    let td7 = document.createElement('td')
-    td7.innerHTML = member.colorHorse
-    tr.appendChild(td7);
-    let td8 = document.createElement('td')
-    td8.innerHTML = member.horseBreed
-    tr.appendChild(td8);
-    let td9 = document.createElement('td')
-    td9.innerHTML = member.city
-    tr.appendChild(td9);
-    let td10 = document.createElement('td')
-    td10.innerHTML = member.competition
-    tr.appendChild(td10);
-    let td11 = document.createElement('td')
-    td11.innerHTML = member.participationEarlier
-    tr.appendChild(td11);
-
+    tr.innerHTML = `<td>${member.firstNameRider} ${member.lastNameRider}</td>
+                      <td>${member.dateBirthdayRider}</td>
+                      <td>${member.phone}</td>
+                      <td>${member.email}</td>
+                      <td>${member.nameHorse}</td>
+                      <td>${member.ageHorse}</td>
+                      <td>${member.colorHorse}</td>
+                      <td>${member.horseBreed}</td>
+                      <td>${member.city}</td>
+                      <td>${member.competition}</td>
+                      <td>${member.participationEarlier}</td>`
     document.getElementById('table').appendChild(tr);
-    })
+});
